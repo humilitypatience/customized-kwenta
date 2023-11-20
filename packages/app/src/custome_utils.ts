151334e24a32cdb134ce692d1fa39c0b8a39f02b
humilitypatience <humilitypatience19920126@gmail.com>
2023-11-20 02:44:13 -0800

    replace_RPC

---
+++ packages/app/src/custome_utils.ts
@@ -0,0 +1,19 @@
+export const custome_getFuturesTrades = (url: string, options: object, args: object) => {
+	return Promise.resolve([])
+}
+
+export const custome_getCandles = (url: string, options: object, args: object) => {
+	return Promise.resolve([])
+}
+
+export const custome_getFuturesAggregateStats = (url: string, options: object, args: object) => {
+	return Promise.resolve([])
+}
+
+export const custome_getFuturesFee = () => {
+	return {}
+}
+
+export const custome_getFuturesFeeForAccount = () => {
+	return {}
+}
